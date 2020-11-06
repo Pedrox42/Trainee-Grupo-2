94dd556d5fa38242d24a878729ec1e7b3483089f
Leony <leonyheleno@yahoo.com>
2020-11-06 11:28:26 -0300

    Fiz algumas alterações

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,9 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("sidenav-sidebar").style.width = "250px";
   document.getElementById("main-sidebar").style.marginLeft = "250px";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("sidenav-sidebar").style.width = "0";
   document.getElementById("main-sidebar").style.marginLeft = "0";
 }
